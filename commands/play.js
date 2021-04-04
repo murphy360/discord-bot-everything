9b78eeb7d25371780b73980cb68493682cff091c
cgacord <cgacord@gmail.com>
2021-04-04 10:56:50 -0500

    Update play.js
    Moved user initialization.

--- commands/play.js
+++ commands/play.js
@@ -2,12 +2,13 @@ module.exports = {
   name: 'play',
   description: 'Play!',
   execute(msg, args) {
+    var user = msg.member.displayName;	
     if(!args[1]){
       msg.reply('Hey ' + user + ', you need to tell me what to play');
       return;
     }	 
 	
-    var user = msg.member.displayName;	  
+  
     console.log(`Sent a reply to ${user}`);	  
     msg.channel.send('Hello ' + user)
 	    .then(() => console.log(`Sent a reply to ${user}`))
