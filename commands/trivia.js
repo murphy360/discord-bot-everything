9318541964d67a6e65c2895cb7ac6a3d8b0bab8c
Corey Murphy <c.murphy360@gmail.com>
2021-04-05 06:03:52 +0000

    nRounds working well enough

--- commands/trivia.js
+++ commands/trivia.js
@@ -5,53 +5,63 @@ module.exports = {
   name: 'trivia',
   description: 'Trivia! based on Open Trivia DB',
   async execute(msg, args) {
-    const amount = args[2];
-    //const category = args[3];
-    //const difficulty = args[4];
-    //const type = args[5];
+    var numRounds = args[2];
 
-    //const { file } = await fetch('https://opentdb.com/api.php?amount='+amount).then(response => response.json());
-    const file = await fetch('https://opentdb.com/api.php?amount='+amount).then(response => response.text());
+    const file = await fetch('https://opentdb.com/api.php?amount='+numRounds).then(response => response.text());
     const REACT=['\u0031\u20E3', '\u0032\u20E3','\u0033\u20E3','\u0034\u20E3'];
     var triviaObject = JSON.parse(file);
-    triviaObject.results[0].incorrect_answers.push(triviaObject.results[0].correct_answer);
-    triviaObject.results[0].incorrect_answers.sort();  
+    
+    function executeRound(triviaObj, roundNumber) {
 
-    var correct_react = ""
+	console.info(triviaObject.results[roundNumber].correct_answer);
+    	triviaObject.results[roundNumber].incorrect_answers.push(triviaObject.results[roundNumber].correct_answer);
+    	triviaObject.results[roundNumber].incorrect_answers.sort();  
 
-    for (let i=0;i<triviaObject.results[0].incorrect_answers.length;i++) {
-      if (triviaObject.results[0].incorrect_answers[i] == triviaObject.results[0].correct_answer) {
-        correct_react = REACT[i];
-      }
-    }
+    	var correct_react = ""
 
-    msg.channel.send(
-	    'Category: ' + triviaObject.results[0].category + '\n' + 
-	    'Difficulty: ' + triviaObject.results[0].difficulty + '\n' + 
-	    'Question: ' + triviaObject.results[0].question).then(sentMsg => {
+    	for (let i=0;i<triviaObject.results[roundNumber].incorrect_answers.length;i++) {
+      		if (triviaObject.results[roundNumber].incorrect_answers[i] == triviaObject.results[roundNumber].correct_answer) {
+        		correct_react = REACT[i];
+      		}
+    	}
 
-	     for (let i=0;i < triviaObject.results[0].incorrect_answers.length;i++) {
+    	msg.channel.send(
+	    'Category: ' + triviaObject.results[roundNumber].category + '\n' + 
+	    'Difficulty: ' + triviaObject.results[roundNumber].difficulty + '\n' + 
+	    'Question: ' + triviaObject.results[roundNumber].question).then(sentMsg => {
+
+	     for (let i=0;i < triviaObject.results[roundNumber].incorrect_answers.length;i++) {
 		sentMsg.react(REACT[i]);
 	     }
              const filter = (reaction, user) => {
-		     console.info(reaction.emoji.name === '\u0031\u20E3' && !user.bot);
 		     return reaction.emoji.name === correct_react && !user.bot;
 	    };
 	     const collector = sentMsg.createReactionCollector(filter, { time: 15000 });
 	     
 	     collector.on('collect', (reaction, user) => {
-		console.info( "Collected" );
                 msg.channel.send(user.username + 'Got the right answer!!!');
 	     });
 
 	     collector.on('end', collected => {
-		console.info( 'Collected ' + collected.size + ' items' );
+		numRounds--;
+		if(numRounds >= 0) {
+			msg.channel.send("\n\n\nNext Round");
+			executeRound(triviaObject, numRounds);
+		}else{
+			msg.channel.send("Game Over");
+		}
 	     });
 	    });
 
-    for (let i=0;i<triviaObject.results[0].incorrect_answers.length;i++) {
-      let j=i+1;
-      msg.channel.send(j + '. ' + triviaObject.results[0].incorrect_answers[i]);
+    for (let i=0; i < triviaObject.results[numRounds].incorrect_answers.length ; i++) {
+      		let j=i+1;
+      		msg.channel.send(j + '. ' + triviaObject.results[numRounds].incorrect_answers[i]);
     }
+	
+    }
+
+  numRounds--;
+  executeRound(triviaObject, numRounds);
+
   },
 };
