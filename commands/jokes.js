b3dbca22bceb9996e2cffe50302c89a507149390
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 12:25:40 -0700

    smarter

--- commands/jokes.js
+++ commands/jokes.js
@@ -10,16 +10,11 @@ module.exports = {
       .then(function(response) { return response.json(); })
       .then(function(json) {
         console.info('Random Joke ' + JSON.stringify(json));
-        var jokeObject = JSON.parse(json);
-        console.info(JSON.stringify(jokeObject));
-        console.info("joke: " + jokeObject.attachments.text);
-        console.info("joke: " + jokeObject.attachments[0].text);
-      msg.channel.send('Random Cat');
-      msg.channel.send(file);	  
-
-      console.info('Random Joke: ' + file);
-      msg.channel.send('Random Joke');
-      msg.channel.send(file);	  
+        
+        console.info("joke: " + json.attachments[0].text);
+        console.info("joke: " + json.attachments.text);
+      //msg.channel.send('Random Cat');
+      //msg.channel.send(file);	  
         });
     
   },
