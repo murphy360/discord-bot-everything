00990c0be62e570eb049ce4c47012cc5c986196b
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 10:46:19 -0700

    adde catch

--- commands/jokes.js
+++ commands/jokes.js
@@ -4,7 +4,10 @@ module.exports = {
   description: 'Random Jokes',
   async execute(msg, args) {
 
-    const { file } = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => response.json());
+    const { file } = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => response.json().catch((error) => {
+      console.info(error);
+      done();
+    }));
     console.info('Random Joke: ' + file);
     msg.channel.send('Random Joke');
     msg.channel.send(file);	  
