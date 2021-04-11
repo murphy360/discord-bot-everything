fcc3cc5aad98849928ae8b506ee0a0951eaf360e
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 10:29:46 -0700

    slack test

--- commands/jokes.js
+++ commands/jokes.js
@@ -9,7 +9,7 @@ module.exports = {
     //const type = args[5];
 
     //const { file } = await fetch('https://opentdb.com/api.php?amount='+amount).then(response => response.json());
-    const { file } = await fetch('https://icanhazdadjoke.com/').then(response => response.json());
+    const { file } = await fetch('https://icanhazdadjoke.com/slack').then(response => response.json());
     console.info('Random Joke');
     msg.channel.send('Random Joke');
     msg.channel.send(file);	  
