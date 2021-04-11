de24d9c1cfa3fd949352b0b6afbbb4ca280958a8
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 07:34:11 -0700

    test

--- commands/trivia.js
+++ commands/trivia.js
@@ -381,7 +381,7 @@ module.exports = {
 			var chaffQuestion1 = triviaObj.results[roundNumber].incorrect_answers[1];
 			var chaffQuestion2 = triviaObj.results[roundNumber].incorrect_answers[2];
 			console.info(correctAnswer);
-			await logQuestion(triviaObj, roundNumber, chaffQuestion0, chaffQuestion1, chaffQuestion2, msg).then(function(result1) {
+			const questionPromise = await logQuestion(triviaObj, roundNumber, chaffQuestion0, chaffQuestion1, chaffQuestion2, msg).then(function(result1) {
 				console.info('lookup: ' + result1);
 				questionId = result1;
 				console.info("Received Question ID: " + questionId);
