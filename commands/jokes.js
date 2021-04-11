fead8b7dd3536fb2d03824f7b6b9f5e12348ef0b
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 10:51:59 -0700

    logging

--- commands/jokes.js
+++ commands/jokes.js
@@ -4,7 +4,7 @@ module.exports = {
   description: 'Random Jokes',
   async execute(msg, args) {
 
-    const { file } = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => JSON.stringify(response));
+    const { file } = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => console.info(JSON.stringify(response)));
     console.info('Random Joke: ' + file);
     msg.channel.send('Random Joke');
     msg.channel.send(file);	  
