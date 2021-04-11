b65a014b190804291ac8237c1f8620f0724d98dd
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 12:00:17 -0700

    jokes

--- commands/jokes.js
+++ commands/jokes.js
@@ -5,13 +5,18 @@ module.exports = {
   description: 'Random Jokes',
   async execute(msg, args) {
     
-    const { file } = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?').then(response => response.json());
-    console.info('Random Cat');
-    msg.channel.send('Random Cat');
-    msg.channel.send(file);	  
+    fetch('http://jsonplaceholder.typicode.com/users', { 
+      method: 'GET'})
+      .then(function(response) { return response.json(); })
+      .then(function(json) {
+        console.info('Random Cat ' + JSON.stringify(json));
+      msg.channel.send('Random Cat');
+      msg.channel.send(file);	  
 
-    console.info('Random Joke: ' + file);
-    msg.channel.send('Random Joke');
-    msg.channel.send(file);	  
+      console.info('Random Joke: ' + file);
+      msg.channel.send('Random Joke');
+      msg.channel.send(file);	  
+        });
+    
   },
 };
