4a4e631bca6a92611ff953b3f442e6da15f60996
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 10:39:09 -0700

    new api

--- commands/jokes.js
+++ commands/jokes.js
@@ -9,8 +9,8 @@ module.exports = {
     //const type = args[5];
 
     //const { file } = await fetch('https://opentdb.com/api.php?amount='+amount).then(response => response.json());
-    const { file } = await fetch('https://icanhazdadjoke.com/slack').then(response => response.json());
-    console.info('Random Joke');
+    const { file } = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => response.json());
+    console.info('Random Joke: ' + file);
     msg.channel.send('Random Joke');
     msg.channel.send(file);	  
   },
