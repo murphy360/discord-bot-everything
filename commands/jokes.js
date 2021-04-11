ebef34b681cd01641c5035b8f33c18c1ed3d4863
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 11:00:53 -0700

    new format

--- commands/jokes.js
+++ commands/jokes.js
@@ -4,8 +4,8 @@ module.exports = {
   name: 'jokes',
   description: 'Random Jokes',
   async execute(msg, args) {
-
-    const file = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => response.json());
+    
+    const { file } = await fetch('https://dad-jokes.p.rapidapi.com/random/joke').then(response => response.json());
     console.info('Random Joke: ' + file);
     msg.channel.send('Random Joke');
     msg.channel.send(file);	  
