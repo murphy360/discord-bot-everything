3ce403137b90688d4ce68857d3a7588d50bb8153
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 10:56:33 -0700

    test

--- commands/jokes.js
+++ commands/jokes.js
@@ -1,10 +1,11 @@
 const fetch = require('node-fetch');
+const { json } = require('sequelize');
 module.exports = {
   name: 'jokes',
   description: 'Random Jokes',
   async execute(msg, args) {
 
-    const file = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => console.info(JSON.stringify(response)));
+    const file = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => response.json());
     console.info('Random Joke: ' + file);
     msg.channel.send('Random Joke');
     msg.channel.send(file);	  
