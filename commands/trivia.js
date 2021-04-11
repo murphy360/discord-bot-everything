ad7d31b0419e8044c00332e1d43cbcd4e4e87e62
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 07:48:11 -0700

    test

--- commands/trivia.js
+++ commands/trivia.js
@@ -381,10 +381,10 @@ module.exports = {
 			var chaffQuestion1 = triviaObj.results[roundNumber].incorrect_answers[1];
 			var chaffQuestion2 = triviaObj.results[roundNumber].incorrect_answers[2];
 			console.info(correctAnswer);
-			await logQuestion(triviaObj, roundNumber, chaffQuestion0, chaffQuestion1, chaffQuestion2, msg).then(
-				function(value) { console.info('Result: ' + value);},
-				function(error) { console.info('error: ' + error);}
-			);
+			await logQuestion(triviaObj, roundNumber, chaffQuestion0, chaffQuestion1, chaffQuestion2, msg).then(value => {
+				console.info('Result: ' + value);
+				questionId = value;
+			});
 			console.info("Received Question ID outside then: " + questionId);
 			
 	    	triviaObj.results[roundNumber].incorrect_answers.push(triviaObj.results[roundNumber].correct_answer);
