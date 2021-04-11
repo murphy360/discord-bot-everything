18db1fefac0eadbd36d112938e111dcc4c259a60
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 10:50:08 -0700

    stringify

--- commands/jokes.js
+++ commands/jokes.js
@@ -4,10 +4,7 @@ module.exports = {
   description: 'Random Jokes',
   async execute(msg, args) {
 
-    const { file } = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => response.json().catch((error) => {
-      console.info(error);
-      done();
-    }));
+    const { file } = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => JSON.stringify(response));
     console.info('Random Joke: ' + file);
     msg.channel.send('Random Joke');
     msg.channel.send(file);	  
