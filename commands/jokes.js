41f2a519370f735518a06e8a00778b623dd5b8e5
Corey Murphy <c.murphy360@gmail.com>
2021-04-11 10:43:22 -0700

    different endpoint

--- commands/jokes.js
+++ commands/jokes.js
@@ -3,12 +3,7 @@ module.exports = {
   name: 'jokes',
   description: 'Random Jokes',
   async execute(msg, args) {
-    //const amount = args[2];
-    //const category = args[3];
-    //const difficulty = args[4];
-    //const type = args[5];
 
-    //const { file } = await fetch('https://opentdb.com/api.php?amount='+amount).then(response => response.json());
     const { file } = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes').then(response => response.json());
     console.info('Random Joke: ' + file);
     msg.channel.send('Random Joke');
