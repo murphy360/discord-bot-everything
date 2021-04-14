9235bb35d4f3e31550d36c5ab039f45b36083777
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 21:55:14 -0700

    working around async

--- commands/find.js
+++ commands/find.js
@@ -12,9 +12,9 @@ module.exports = {
     } else if (args[2] === 'pokemon'){
       console.info('find pokemon');
       //const pokemonId = Math.floor(Math.random() * 897) + 1;
-      const { json } = await fetch("https://app.pokemon-api.xyz/pokemon/random").then(response => response.text());
+      const { json } = await fetch("https://app.pokemon-api.xyz/pokemon/random").then(response => response.text()).then(msg.channel.send(json));
       console.info('Random Pokemon');
-      msg.channel.send(json);	  
+       
     } else if (args[2] === 'cat') {
       const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
       console.info('Random Cat');
