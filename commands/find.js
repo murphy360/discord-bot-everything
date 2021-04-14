289eb4222ae7f5e3590c43078ecd6e0f8a3ea093
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 21:32:46 -0700

    trying agian

--- commands/find.js
+++ commands/find.js
@@ -12,14 +12,9 @@ module.exports = {
     } else if (args[2] === 'pokemon'){
       console.info('find pokemon');
       const pokemonId = Math.floor(Math.random() * 897) + 1;
-      fetch("https://pokemon-go1.p.rapidapi.com/pokemon_stats.json", {
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-key": "9ded9c1174mshe1831826938f2f2p12da78jsnc5deaf9eb058",
-          "x-rapidapi-host": "pokemon-go1.p.rapidapi.com"
-        }
-      })
+      fetch("https://pokeapi.co/api/v2/pokemon/"+pokemonId)
       .then(response => { 
+        console.info(response);
         response.json() })
       .then(json => {
         //pokemonJson = await response.json(); 
