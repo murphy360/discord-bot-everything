7bfb0f8a6211e05475901e930935e6a3752e67d5
Corey Murphy <c.murphy360@gmail.com>
2021-04-14 05:41:50 -0700

    Clean Code!

--- commands/find.js
+++ commands/find.js
@@ -8,48 +8,44 @@ module.exports = {
   name: 'find',
   description: 'Finds things',
   async execute(msg, args) {
-    console.info("Inside find ");
+    
+    async function sendEmbedMessage(message, json) {
+      //create and format 
+      const messageEmbed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor('Found a wild!')
+      //.addFields({name: 'Type', json.type})
+      .setTitle(json.name.english)
+      .setThumbnail(he.decode(json.hires))
+      .addFields(
+        {name:"Species",value:json.species},
+        {name:"Type",value:json.type},
+        {name:"HP",value:json.base.HP,inline:true},
+        {name:"Defense",value:json.base.Defense,inline:true},
+        {name:"Attack",value:json.base.Attack,inline:true},
+        {name:"Sp. Attack",value:json.base['Sp. Attack'],inline:true},
+        {name:"Sp. Defense",value:json.base['Sp. Defense'],inline:true},
+        {name:"Speed",value:json.base.Speed,inline:true},
+        {name:"Description",value:json.description}
+      )
+      .setFooter("API provided by: https://purukitto.github.io/pokemon-api/")
+        message.channel.send(messageEmbed);
+    }
+    //if command is only "@bot find" then:
     if (!args[2]){
       msg.channel.send("What should I find?");
+      //@bot find pokemon
     } else if (args[2] === 'pokemon'){
-      console.info('find pokemon');
-      //const pokemonId = Math.floor(Math.random() * 897) + 1;
+      
+      //Make an API call for a single random Pokemon
       await fetch("https://app.pokemon-api.xyz/pokemon/random")
       .then(response => response.text())
+      //create a JSON object
       .then(json => JSON.parse(json))
-      //.then(json => msg.channel.send(json))
-      .then(json => {
+      .then(json => sendEmbedMessage(msg, json)); 
 
-        console.info(json.hires);
-        //let imageUrl = json.sprite.split('hires');
-        //console.info(imageUrl[0]);
-        //console.info(imageUrl[1]);
-       
-        const messageEmbed = new Discord.MessageEmbed()
-          .setColor('#0099ff')
-          .setAuthor('Found a wild!')
-          //.addFields({name: 'Type', json.type})
-          .setTitle(json.name.english)
-          .setThumbnail(he.decode(json.hires))
-          .addFields(
-            {name:"Species",value:json.species},
-            {name:"Type",value:json.type},
-            {name:"HP",value:json.base.HP,inline:true},
-            {name:"Defense",value:json.base.Defense,inline:true},
-            {name:"Attack",value:json.base.Attack,inline:true},
-            {name:"Sp. Attack",value:json.base['Sp. Attack'],inline:true},
-            {name:"Sp. Defense",value:json.base['Sp. Defense'],inline:true},
-            {name:"Speed",value:json.base.Speed,inline:true},
-            {name:"Description",value:json.description}
-          )
-          .setFooter("Data provided by: https://purukitto.github.io/pokemon-api/")
-          msg.channel.send(messageEmbed);
-      });
-      console.info('Random Pokemon');
-       
     } else if (args[2] === 'cat') {
       const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-      console.info('Random Cat');
       msg.channel.send('Random Cat');
       msg.channel.send(file);	  
       
