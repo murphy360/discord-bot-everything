95af49a864f2cb52f18949f309d9237f5b738b5a
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 20:10:13 -0700

    var wasn't working

--- commands/find.js
+++ commands/find.js
@@ -5,10 +5,10 @@ module.exports = {
   description: 'Finds things',
   async execute(msg, args) {
     let findCommand = args[2];
-    console.info("Inside find " + findCommand);
-    if (!findCommand){
+    console.info("Inside find " + args[2]);
+    if (!args[2]){
       msg.channel.send("What should I find?");
-    } if (findCommand === 'pokemon'){
+    } if (args[2] === 'pokemon'){
       const pokemonId = rand(1, 898);
       const { file } = await fetch('https://api.pokemon.com/us/pokedex/'+pokemonId).then(response => response.json());
       console.info('Random Pokemon');
