e868ef33b3c8f91e9bd1601cea6ed69c873afb4f
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 18:06:07 -0700

    changed function calls

--- commands/joke.js
+++ commands/joke.js
@@ -6,8 +6,8 @@ module.exports = {
   async execute(msg, args) {
     fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes', { 
       method: 'GET'})
-      .then(function(response) { return response.json(); })
-      .then(function(json) {
+      .then(response => { return response.json(); })
+      .then(json => {
         msg.channel.send(json.setup);
         msg.channel.send(json.punchline);
         });
