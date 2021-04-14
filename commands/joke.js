2e02c9fa568243db114d9b67151aeb21abc0738b
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 18:20:55 -0700

    Timer to 10s

--- commands/joke.js
+++ commands/joke.js
@@ -9,7 +9,7 @@ module.exports = {
       .then(response => { return response.json(); })
       .then(json => {
         msg.channel.send(json.setup);
-        setTimeout(() => msg.channel.send(json.punchline), 4000);
+        setTimeout(() => msg.channel.send(json.punchline), 10000);
         });
   },
 };
