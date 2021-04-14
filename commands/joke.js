86df7ca53752c013ef4cfca122f2f151f85d827d
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 18:02:17 -0700

    yeah no that didn't work... Just chaning the API

--- commands/joke.js
+++ commands/joke.js
@@ -4,12 +4,12 @@ module.exports = {
   name: 'joke',
   description: 'Random Joke',
   async execute(msg, args) {
-    
-    fetch('https://icanhazdadjoke.com/slack', { 
+    fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes', { 
       method: 'GET'})
       .then(function(response) { return response.json(); })
       .then(function(json) {
-        msg.channel.send(json.attachments[0].text);
+        msg.channel.send(json.setup);
+        msg.channel.send(json.punchline);
         });
   },
 };
