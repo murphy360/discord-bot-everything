cb2a1b01fc3f5c9638473a16e5ac711e9fb3e433
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 21:52:56 -0700

    formatting

--- commands/find.js
+++ commands/find.js
@@ -12,8 +12,7 @@ module.exports = {
     } else if (args[2] === 'pokemon'){
       console.info('find pokemon');
       //const pokemonId = Math.floor(Math.random() * 897) + 1;
-      const { json } = await fetch("https://app.pokemon-api.xyz/pokemon/random");
-      json.then(response => response.text());
+      const { json } = await fetch("https://app.pokemon-api.xyz/pokemon/random").then(response => response.text());
       console.info('Random Pokemon');
       msg.channel.send(json);	  
     } else if (args[2] === 'cat') {
