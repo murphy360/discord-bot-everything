f6e7cf7cba299a00b73fe0ee144cdf67ea9c19d3
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 20:24:15 -0700

    meh

--- commands/find.js
+++ commands/find.js
@@ -4,11 +4,10 @@ module.exports = {
   name: 'find',
   description: 'Finds things',
   async execute(msg, args) {
-    let findCommand = args[2];
-    console.info("Inside find " + args[2]);
+    console.info("Inside find ");
     if (!args[2]){
       msg.channel.send("What should I find?");
-    } if (args[2] === 'pokemon'){
+    } else if (args[2] === 'pokemon'){
       const pokemonId = rand(1, 898);
       const { file } = await fetch('https://api.pokemon.com/us/pokedex/'+pokemonId).then(response => response.json());
       console.info('Random Pokemon');
