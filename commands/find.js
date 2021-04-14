6069ebe76a41e5fb5393b221c37ee93446bbc4c5
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 19:49:27 -0700

    adding call to pokemon api

--- commands/find.js
+++ commands/find.js
@@ -4,9 +4,15 @@ module.exports = {
   name: 'find',
   description: 'Finds things',
   async execute(msg, args) {
-
+    let findCommand = args[2];
     if (!args[2]){
       msg.channel.send("What should I find?");
+    } if (findCommand === 'pokemon'){
+      const pokemonId = rand(1, 898);
+      const { file } = await fetch('https://api.pokemon.com/us/pokedex/'+pokemonId).then(response => response.json());
+      console.info('Random Pokemon');
+      console.info(JSON.stringify(file));
+      msg.channel.send(file);	  
     }
     
   },
