fe4d247a332e635cbb6cec4efced998fc023a308
Corey Murphy <c.murphy360@gmail.com>
2021-04-13 21:13:42 -0700

    New API

--- commands/find.js
+++ commands/find.js
@@ -12,12 +12,19 @@ module.exports = {
     } else if (args[2] === 'pokemon'){
       console.info('find pokemon');
       const pokemonId = Math.floor(Math.random() * 897) + 1;
-      fetch('https://pokeapi.co/api/v2/pokemon/'+pokemonId).then(response => {
-        console.info('got something');
-        //console.info(response);
-        console.info(JSON.stringify(response));
-        msg.channel.send(response.name);	 
-      });   
+      fetch("https://pokemon-go1.p.rapidapi.com/pokemon_stats.json", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-key": "9ded9c1174mshe1831826938f2f2p12da78jsnc5deaf9eb058",
+          "x-rapidapi-host": "pokemon-go1.p.rapidapi.com"
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.error(err);
+      });
     }
   },
 };
