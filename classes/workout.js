5c85276c2566d08c8aad719c0346866a35ac34cc
Corey Murphy <c.murphy360@gmail.com>
2021-04-19 09:07:45 -0700

    better icon

--- classes/workout.js
+++ classes/workout.js
@@ -18,6 +18,7 @@ class Workout {
         this.isValid=false 
         this.EXERCISES=[] 
         this.INTERVAL
+        this.icon='https://previews.123rf.com/images/kongvector/kongvector2003/kongvector200300022/141391692-independence-day-drum-mascot-icon-on-fitness-exercise-trying-barbells-vector-illustration.jpg'
         this.currentSet=0                     
         for (let i = 4; i < args.length ; i+=2) {
             
@@ -64,7 +65,7 @@ class Workout {
                             .setTitle(this.MESSAGE.author.username + "Has started a workout!")
                             .setColor("#0099ff")
                             .setDescription(workoutDetailsString)
-                            .setThumbnail('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.brandcrowd.com%2Fmaker%2Flogo%2Frobot-gym-fitness-tech-67661&psig=AOvVaw2q-bcbw4lbA3U41mggjhTy&ust=1618934479040000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKD2x43XivACFQAAAAAdAAAAABAD')
+                            .setThumbnail(this.icon)
                             .addFields(
                                 {name: "Exercise", value: exerciseString, inline: true},
                                 {name: "Reps", value: repString, inline: true}
@@ -87,7 +88,7 @@ class Workout {
                             .setTitle(roundString)
                             .setColor("#0099ff")
                             .setDescription(descriptionString) 
-                            .setThumbnail('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.brandcrowd.com%2Fmaker%2Flogo%2Frobot-gym-fitness-tech-67661&psig=AOvVaw2q-bcbw4lbA3U41mggjhTy&ust=1618934479040000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKD2x43XivACFQAAAAAdAAAAABAD')
+                            .setThumbnail(this.icon)
                             .addFields(
                                 {name: "Exercise", value: exerciseString, inline: true},
                                 {name: "Reps", value: repString, inline: true}
@@ -109,8 +110,7 @@ class Workout {
         const finishedDetails = new Discord.MessageEmbed()
                             .setTitle("Workout Complete!")
                             .setColor("#0099ff")
-                            .setDescription(descriptionString)
-                            .setThumbnail('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.brandcrowd.com%2Fmaker%2Flogo%2Frobot-gym-fitness-tech-67661&psig=AOvVaw2q-bcbw4lbA3U41mggjhTy&ust=1618934479040000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKD2x43XivACFQAAAAAdAAAAABAD')
+                            .setThumbnail(this.icon)
                             .addFields(
                                 {name: "Exercise", value: exerciseString, inline: true},
                                 {name: "Reps", value: repString, inline: true}
