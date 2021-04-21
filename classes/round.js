473f2fe2d10249d2d2fab59657b90a935680532d
Christian Acord <15983047+cgacord@users.noreply.github.com>
2021-04-21 00:12:32 -0400

    create round class
    #42

---
+++ classes/round.js
@@ -0,0 +1,67 @@
+const Question = require('./question.js');
+const Timer = require('./timer.js');
+const Discord = require('Discord');
+const Reactions=['\u0031\u20E3', '\u0032\u20E3','\u0033\u20E3','\u0034\u20E3'];
+
+class Round {
+    
+    constructor(question, round_num, channel) {
+        this.winner = null;
+        this.players = null;
+        this.question = question;
+        this.channel = channel;
+        this.question_message = null;
+        this.time = 60;
+        this.timer = null;
+        this.stated_at = null;
+    }
+    
+    play() {
+        this.started_at = Date.now();
+        this.channel.send('Round ' + round_num + ' starting');
+        this.presentQuestion();
+        this.collectUserReactions();       
+    }
+    
+    presentQuestion() {
+        this.question_message = this.question.display(this.channel);
+        
+        for (let i = 0; i < this.question.num_choices; i++) {
+            // add reactions
+            this.question_message.react(Reaction[i]);
+        }
+        
+        this.timer = new Timer.Timer(this.time,5,this.channel,'Time Remaining').start();
+        
+    }
+    
+    collectUserReactions() {
+        // use collector and filters
+    }
+    
+    getQuestionTimeLength() {
+        switch (this.question.difficulty) {
+            case 'easy': 
+                this.time = 30;
+                break;
+            case 'medium': 
+                this.time = 45;
+                break;
+            default: 
+                this.time = 60;
+                break;
+        }
+    }
+    
+    end() {
+    }
+    
+    logRound() {
+    }
+    
+    getWinner() {
+        return this.winner;
+    }
+}
+
+module.exports.Round = Round
