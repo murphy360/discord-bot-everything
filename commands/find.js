7723efae8672a022ed22aa01151d36b38295ef49
Christian Acord <15983047+cgacord@users.noreply.github.com>
2021-04-21 17:08:54 -0400

    Update find.js

--- commands/find.js
+++ commands/find.js
@@ -18,23 +18,22 @@ module.exports = {
 	}
 
       const messageEmbed = new Discord.MessageEmbed()
-      .setColor(FIND_COLOR)      // Changed the color to the Pokemon yellow (just for fun)
+      .setColor(FIND_COLOR)
       .setAuthor('Found in the wild!')
-      //.addFields({name: 'Type', json.type})
       .setTitle(json.name.english)
       .setURL("https://www.pokemon.com/us/pokedex/"+json.name.english)
       .setDescription(json.description)
       .setThumbnail(he.decode(json.hires))
       .addFields(
-	      {name: "__Species__"+"\u0009\u0009\u0009\u0009"+"__Type__", value:json.species+"\u0009\u0009\u0009\u0009"+json.type, inline:true}/*,
-	      {name: "__Type__", value:"\u0009"+json.type, inline:true},
-	      {name: "__Ability__", value:"\u0009"+ability, inline:true},
-	      {name: "__HP__", value:json.base.HP,inline:true},
-              {name: "__Defense__", value:json.base.Defense, inline:true},
-              {name: "__Attack__", value:json.base.Attack, inline:true},
-              {name: "__Sp. Attack__", value:json.base['Sp. Attack'], inline:true},
-              {name: "__Sp. Defense__", value:json.base['Sp. Defense'], inline:true},
-              {name: "__Speed__", value:json.base.Speed,inline:true}, */
+	      {name: "__Species__", value: json.species, inline: true},
+	      {name: "__Type__", value: json.type, inline: true},
+	      {name: "__Ability__", value: ability, inline: true},
+	      {name: "__HP__", value: json.base.HP, inline: true},
+              {name: "__Defense__", value: json.base.Defense, inline: true},
+              {name: "__Attack__", value: json.base.Attack, inline: true},
+              {name: "__Sp. Attack__", value: json.base['Sp. Attack'], inline: true},
+              {name: "__Sp. Defense__", value: json.base['Sp. Defense'], inline: true},
+              {name: "__Speed__", value: json.base.Speed, inline: true}
       )
       .setFooter("Pokemon data provided by: https://purukitto.github.io/pokemon-api/")
         message.channel.send(messageEmbed);
@@ -46,7 +45,7 @@ module.exports = {
     } else if (args[2] === 'pokemon'){
       
       //Make an API call for a single random Pokemon
-      await fetch("https://app.pokemon-api.xyz/pokemon/random")
+      fetch("https://app.pokemon-api.xyz/pokemon/random")
       .then(response => response.text())
       //create a JSON object
       .then(json => JSON.parse(json))
