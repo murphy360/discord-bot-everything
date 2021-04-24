f50bd0f8ed7f77aabaeb5f5ee292b7484b773b13
Corey Murphy <c.murphy360@gmail.com>
2021-04-23 23:45:54 -0700

    working around async

--- commands/workout.js
+++ commands/workout.js
@@ -11,16 +11,18 @@ module.exports = {
     //args[3...n] = exercises
     //args[3+1..n+1] = reps
     let workout = new Workout(msg, args)
-    await workout.buildExerciseList(args)
+    await workout.buildExerciseList(args).then(valid => {
+      if (valid) {
+        workout.messageWorkoutDetails()
+        workout.startWorkout()
+      } else { 
+        msg.channel.send('The Workout is not valid')
+      }    
+    })
     console.info("Workout command post new Workout")
    
 
-    if (workout.isValid) {
-      workout.messageWorkoutDetails()
-      workout.startWorkout()
-    } else { 
-      msg.channel.send('The Workout is not valid')
-    }    
+    
     
      
     	  
