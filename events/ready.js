14484097088ac3349db3caa17b27ac919630a5a7
Corey Murphy <c.murphy360@gmail.com>
2023-06-10 01:29:30 -0700

    Bot logs startup to dev guild

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,8 @@ require('dotenv').config({ path: './../data/.env' });
 const TRIVIA_CHANNEL = process.env.TRIVIA_CHANNEL;
 const CHAT_GPT_CHANNEL = process.env.CHAT_GPT_CHANNEL;
 const DEV = process.env.DEV;
+const DEV_GUILD_ID = process.env.DEV_GUILD_ID;
+const playerRoleName = process.env.PLAYER_ROLE;
 
 const { Events } = require ('discord.js');
 
@@ -57,11 +59,28 @@ module.exports = {
                 console.info('Chat GPT Channel Exists: ' + chatChannel.name);
             }
 
-            if (DEV == "true"){
-                console.info('DEV MODE - Skipping Greeting');
-                return;
-            } else {
-                defaultChannel.send(greetings[Math.floor((Math.random()*greetings.length))]);
+            if (guild.id == DEV_GUILD_ID){
+                const devChannel = await guild.channels.cache.find(channel => channel.name === "trivia_bot");
+                devChannel.send(greetings[Math.floor((Math.random()*greetings.length))]);
+            }
+            
+            // Check if role exists
+            let playerRole = await guild.roles.cache.find(role => role.name === playerRoleName);
+            
+            // Create role if it doesn't exist
+            if (!playerRole) {
+                console.info(guild.name + ': Role ' + playerRoleName + ' does not exist, creating it now');
+                // Create Player role
+                await guild.roles.create({
+                    name: playerRoleName,
+                    color: '#00ff00', // Green
+                    hoist: true,
+                    position: 105,
+                }).then(async role => {
+                    console.info(guild.name + ': Created role ' + role.name);
+                }).catch(console.error); 
+            }  else {
+                console.info(guild.name + ': Checking if role ' + playerRoleName + ' exists');
             }
         });        
     },
