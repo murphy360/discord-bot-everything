826499b3fe610c3f6da13db31bf915035a77bc31
Corey Murphy <c.murphy360@gmail.com>
2023-08-19 17:35:35 -0700

    Send multiple messages instead

--- commands/developer/listguilds.js
+++ commands/developer/listguilds.js
@@ -16,13 +16,17 @@ module.exports = {
 			await interaction.reply({ content: "You are not authorized to use this command.", ephemeral: true });
 			return;
 		}
-
+		interaction.deferReply();
 		const guilds = await interaction.client.guilds.cache;
 		console.info("Guilds: " + guilds.size);
 		let triviaGuilds = await this.getTriviaGuilds(guilds);
 		console.info("Trivia Guilds: " + triviaGuilds.length);
-		let guildEmbed = await this.getGuildsEmbed(triviaGuilds);
-		await interaction.reply({ embeds: [guildEmbed] });
+		interaction.editReply("Here you go!");
+		for (let i = 0; i < triviaGuilds.length; i++) {
+			console.log(triviaGuilds[i].guild.name);
+			let guildEmbed = await this.getGuildsEmbed(triviaGuilds[i]);
+			await interaction.channel.send({ embeds: [guildEmbed] });
+		}
 	},
 
 	async getTriviaGuilds(guilds) {
@@ -37,61 +41,55 @@ module.exports = {
 	return triviaGuilds;
 	},
 
-	async getGuildsEmbed(triviaGuilds) {
+	async getGuildsEmbed(triviaGuild) {
 		console.info('getGuildsEmbed');
 		let guildEmbed = new EmbedBuilder()
-			.setTitle('Guilds')
-			.setDescription('List of guilds the bot is in')
+			.setTitle(triviaGuild.guild.name)
+			.setDescription('Status of Don\'t Panic! in this guild.')
 			.setColor('#0099ff')
 			.setTimestamp();
 
-		console.log(triviaGuilds.length);
-		let guildString = "";
+		await triviaGuild.checkGuildCriticalSetup();
+		await triviaGuild.setGuildTriviaUsers();
 
-		for (let i = 0; i < triviaGuilds.length; i++) {
-			await triviaGuilds[i].checkGuildCriticalSetup();
-			await triviaGuilds[i].setGuildTriviaUsers();
-			console.log(triviaGuilds[i].guild.name);
-			let triviaChannelName = "None";
-			let channelInvite = "None";
+		let triviaChannelName = "None";
+		let channelInvite = "None";
 
-			// check if channel is set
-			if (triviaGuilds[i].triviaChannel != null) {
-				triviaChannelName = triviaGuilds[i].triviaChannel.name;
-			}
+		// check if channel is set
+		if (triviaGuild.triviaChannel != null) {
+			triviaChannelName = triviaGuild.triviaChannel.name;
+		}
 
-			// check if client has permission to create invite
-			if (!triviaGuilds[i].guild.members.me.permissions.has(PermissionsBitField.Flags.CreateInstantInvite)) {
-				channelInvite = "No Permission";
-			} else if (triviaGuilds[i].triviaChannel == null) {
-				channelInvite = "No Channel";
-			} else {
-				await triviaGuilds[i].triviaChannel.createInvite({ unique: true, temporary: false }).then(invite => {
-					console.log(invite.code);
-					channelInvite = "https://discord.gg/" + invite.code;
-				  });
-			}
-			let triviaUsers = "0";
-			if (triviaGuilds[i].allGuildPlayers != null) {
-				triviaUsers = triviaGuilds[i].allGuildPlayers.length.toString();
-			}
+		// check if client has permission to create invite
+		if (!triviaGuild.guild.members.me.permissions.has(PermissionsBitField.Flags.CreateInstantInvite)) {
+			channelInvite = "No Permission";
+		} else if (triviaGuild.triviaChannel == null) {
+			channelInvite = "No Channel";
+		} else {
+			await triviaGuild.triviaChannel.createInvite({ unique: true, temporary: false }).then(invite => {
+				console.log(invite.code);
+				channelInvite = "https://discord.gg/" + invite.code;
+				});
+		}
+		let triviaUsers = "0";
+		if (triviaGuild.allGuildPlayers != null) {
+			triviaUsers = triviaGuild.allGuildPlayers.length.toString();
+		}
 
-			if (triviaGuilds[i].isReady) {
-				guildString += "✅ - " + triviaGuilds[i].guild.name + "\n";
-			} else {
-				guildString += "❌ - " + triviaGuilds[i].guild.name + "\n";
-			}
-			guildString += "Channel: " + triviaChannelName + ", Invite: " + channelInvite + "\n\n";
-			//guildString += "Invite: " +  + "\n";
-			//guildString += "Members: " + triviaGuilds[i].guild.memberCount.toString() + "\n";
-			//guildString += "Trivia Players: " + triviaUsers + "\n";
-			//guildString += "\n";
-			
-
+		if (triviaGuild.isReady) {
+			guildEmbed.addFields(
+				{ name: "✅ - " + triviaChannelName, value: channelInvite, inline: true }
+			);
+		} else {
+			guildEmbed.addFields(
+				{ name: "❌ - " + triviaChannelName, value: channelInvite, inline: true }
+			);
 		}
 
 		guildEmbed.addFields(
-			{ name: "Guilds", value: guildString, inline: false },
+			{ name: triviaChannelName, value: channelInvite, inline: true },
+			{ name: 'Members', value: triviaGuild.guild.memberCount.toString(), inline: true },
+			{ name: 'Trivia Players', value: triviaUsers, inline: true }
 		);
 		return guildEmbed;
 	},
